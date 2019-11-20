aa91ffc253245d3a849d68fa49baefb99cfcd362
Lilla Kovacs <kovilil@gmail.com>
2019-11-20 02:52:45 +0100

    first running test

--- cypress/integration/version-1-tests/loginUI.spec.js
+++ cypress/integration/version-1-tests/loginUI.spec.js
@@ -1,5 +1,13 @@
 /// <reference types="Cypress" />
 
-beforeEach(() => {
+before(() => {
   cy.visit("https://demo.applitools.com/hackathon.html");
+});
+
+it("header should display the correct text", () => {
+  cy.get(".auth-header").should("include.text", "Login Form");
+});
+
+it("username placeholder should display the correct text", () => {
+  cy.get("#username.form-control").should("have.text", "username");
 });
