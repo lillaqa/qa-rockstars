afc0a157c5bed319bc8c8b9cefcaa8f02e29448d
Lilla Kovacs <kovilil@gmail.com>
2019-11-20 03:17:10 +0100

    fix context part

--- cypress/integration/version-1-tests/data-driven-test.spec.js
+++ cypress/integration/version-1-tests/data-driven-test.spec.js
@@ -1,42 +1,45 @@
 /// <reference types="Cypress" />
 
-describe("unhappy flow", () => {
+context("Login data-driven tests", () => {
   beforeEach(() => {
     cy.visit("https://demo.applitools.com/hackathon.html");
   });
-  it("empty fields should throw an error", () => {
-    cy.get("#log-in").click();
 
-    cy.get(".alert.alert-warning").should(
-      "have.text",
-      "Both Username and Password must be present "
-    );
+  describe("unhappy flow", () => {
+    it("empty fields should throw an error", () => {
+      cy.get("#log-in").click();
+
+      cy.get(".alert.alert-warning").should(
+        "have.text",
+        "Both Username and Password must be present "
+      );
+    });
+
+    it("only username should throw an error", () => {
+      cy.get("#username").type("username");
+      cy.get("#log-in").click();
+      cy.get(".alert.alert-warning").should(
+        "have.text",
+        "Password must be present"
+      );
+    });
+
+    it("only password should throw an error", () => {
+      cy.get("#password").type("password");
+      cy.get("#log-in").click();
+      cy.get(".alert.alert-warning").should(
+        "have.text",
+        "Username must be present"
+      );
+    });
   });
 
-  it("only username should throw an error", () => {
-    cy.get("#username").type("username");
-    cy.get("#log-in").click();
-    cy.get(".alert.alert-warning").should(
-      "have.text",
-      "Password must be present"
-    );
-  });
-
-  it("only password should throw an error", () => {
-    cy.get("#password").type("password");
-    cy.get("#log-in").click();
-    cy.get(".alert.alert-warning").should(
-      "have.text",
-      "Username must be present"
-    );
-  });
-});
-
-describe("happy flow", () => {
-  it("successful login", () => {
-    cy.get("#username").type("username");
-    cy.get("#password").type("password");
-    cy.get("#log-in").click();
-    cy.url().should("include", "/hackathonApp.html");
+  describe("happy flow", () => {
+    it("successful login", () => {
+      cy.get("#username").type("username");
+      cy.get("#password").type("password");
+      cy.get("#log-in").click();
+      cy.url().should("include", "/hackathonApp.html");
+    });
   });
 });
