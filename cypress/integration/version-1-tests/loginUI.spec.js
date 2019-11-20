3f8b79bc9a9c4f46f2b4c71f9a5d0abe27f5584e
Lilla Kovacs <kovilil@gmail.com>
2019-11-20 02:54:14 +0100

    remove test case

--- cypress/integration/version-1-tests/loginUI.spec.js
+++ cypress/integration/version-1-tests/loginUI.spec.js
@@ -7,7 +7,3 @@ before(() => {
 it("header should display the correct text", () => {
   cy.get(".auth-header").should("include.text", "Login Form");
 });
-
-it("username placeholder should display the correct text", () => {
-  cy.get("#username.form-control").should("have.text", "username");
-});
