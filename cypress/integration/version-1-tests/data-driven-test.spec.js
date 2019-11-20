fb9821a617c6fd41a4f7872c82542f3627180909
Lilla Kovacs <kovilil@gmail.com>
2019-11-20 02:29:17 +0100

    add the first iterations of the data-driven tests

--- cypress/integration/version-1-tests/data-driven-test.spec.js
+++ cypress/integration/version-1-tests/data-driven-test.spec.js
@@ -12,3 +12,28 @@ it("empty fields should throw an error", () => {
     "Both Username and Password must be present "
   );
 });
+
+it("only username should throw an error", () => {
+  cy.get("#username").type("username");
+  cy.get("#log-in").click();
+  cy.get(".alert.alert-warning").should(
+    "have.text",
+    "Password must be present"
+  );
+});
+
+it("only password should throw an error", () => {
+  cy.get("#password").type("password");
+  cy.get("#log-in").click();
+  cy.get(".alert.alert-warning").should(
+    "have.text",
+    "Username must be present"
+  );
+});
+
+it("successful login", () => {
+  cy.get("#username").type("username");
+  cy.get("#password").type("password");
+  cy.get("#log-in").click();
+  cy.url().should("include", "/hackathonApp.html");
+});
