110704df78a25c0a5903a5c790f7fdb62c5e44c7
Lilla Kovacs <kovilil@gmail.com>
2019-11-23 22:14:59 +0100

    add label assertions

--- cypress/integration/v1/TraditionalLoginUI.test.js
+++ cypress/integration/v1/TraditionalLoginUI.test.js
@@ -9,5 +9,17 @@ context("Login UI tests", () => {
     it("header should display the correct text", () => {
       cy.get(".auth-header").should("include.text", "Login Form");
     });
+
+    it("username label should be correct", () => {
+      cy.get("label").contains("Username");
+    });
+
+    it("password label should be correct", () => {
+      cy.get("label").contains("Password");
+    });
+
+    it("checkbox label should be correct", () => {
+      cy.get("label").contains("Remember Me");
+    });
   });
 });
