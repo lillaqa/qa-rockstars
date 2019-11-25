1e6a0639f8f16fd2340edfaf748e20bfc3e402b4
Lilla Kovacs <kovilil@gmail.com>
2019-11-25 03:33:08 +0100

    init file

---
+++ cypress/integration/v1/TraditionalTableSort.test.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+import { LoginPage } from "../v1/page-objects/login-page";
+
+context("Table sorting tests", () => {
+  const loginPage = new LoginPage();
+  beforeEach(() => {
+    loginPage.login();
+  });
+
+  describe("testing the default view", () => {});
+});
